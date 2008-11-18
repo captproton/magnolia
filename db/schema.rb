3d9f7f86ad5dff5f7f68568e4cebe04cb0b2d4e0
Jesse Clark <jesse@jesseclark.com>
2008-11-18 11:04:00 -0800

    Removing AuthenticationProvider files in preparation for single field sign-on

--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081110184646) do
-
-  create_table "authentication_providers", :force => true do |t|
-    t.string   "name",             :default => "",                       :null => false
-    t.string   "type",             :default => "AuthenticationProvider", :null => false
-    t.string   "label"
-    t.string   "logo"
-    t.string   "button"
-    t.text     "description"
-    t.boolean  "active",           :default => true,                     :null => false
-    t.integer  "display_sequence"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20081106212028) do
 
   create_table "users", :force => true do |t|
     t.string   "screen_name",            :limit => 50
